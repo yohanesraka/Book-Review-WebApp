d1781593d3ec315e9b75fe0d9cadfcab8826f09c
Yohanes Raka <61136676+yohanesraka@users.noreply.github.com>
2022-01-20 22:58:50 +0700

    Update application.py

--- application.py
+++ application.py
@@ -10,21 +10,21 @@ from passlib.hash import sha256_crypt
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"Rq45z\n\xec]/'
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)    
+    
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv(uri))
 db = scoped_session(sessionmaker(bind=engine))
 
 #Index route
